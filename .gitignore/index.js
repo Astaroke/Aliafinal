279ed05131aa75ed08470fb2f9545a041b0daabc
Astaroke <61854317+Astaroke@users.noreply.github.com>
2020-03-06 04:08:41 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,4 +7,4 @@ bot.on("ready", function() {
     console.log("Bonjour maitre");
 });
 
-bot.login("Njg1MjM3Njg0NjYyNTY3MDc1.XmG9gA.OnYyEPRilZQZI3LJmjTx1g-L2y4");
+bot.login("Njg1MjM3Njg0NjYyNTY3MDc1.XmG-pQ.AMI5Daz2LY8GZIYUgvLNpXh-ar4");

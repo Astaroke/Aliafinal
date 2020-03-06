3655398b261236abb1136713c8b90e10dd5dcfdf
Astaroke <61854317+Astaroke@users.noreply.github.com>
2020-03-06 03:31:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,6 @@ var bot = new Discord.Client();
 bot.on("ready", function() {
     bot.user.setGame("Dofus, !help");
     console.log("Bonjour maitre");
-})
+});
 
 bot.login("Njg1MjM3Njg0NjYyNTY3MDc1.XmGrpA.9PYm82mg4libAys-ei2U_akjAgY");
